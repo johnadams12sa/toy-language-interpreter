5aa7a2f3a551aa6eeca2bc0a143520d12e5b3573
Aaron Yam <ayam5005@gmail.com>
2019-05-06 16:46:31 -0400

    Update lex.py
    Added unary operators and found new error with regular expression for numbers. Unable to read a multidigit number as is, instead reads it as individual numbers. Added labels for each section

--- lex.py
+++ lex.py
@@ -5,14 +5,19 @@ from rply import LexerGenerator
 lg = LexerGenerator()
 
 #LIST OF TOKENS
+#Unary Operators
+lg.add("NEGATE", r"\-")
+lg.add("POSITIVE", r"+")
+#Binary Operators
 lg.add("PLUS", r"\+")
 lg.add("MINUS", r"\-")
 lg.add("MULTIPLY", r"\*")
-lg.add("DIVIDE", r"\/")
+#Parenthesis
 lg.add("LPAREN", r"\(")
 lg.add("RPAREN", r"\)")
+#Errors reading multidigit numbers
 lg.add("NUMBER", r"[[0-9]|[1-9][0-9]*]")
-lg.add("=", r"=")
+lg.add("EQUAL", r"=")
 lg.add("IDENTIFIER", r"[a-zA-z_][a-zA-Z0-9_]*")
 lg.ignore(r"[\t\s]+")
 
