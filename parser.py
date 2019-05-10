0db70d9d7aab0e808c33ca9abb6bc7f78764a0af
Aaron Yam <ayam5005@gmail.com>
2019-05-09 23:57:36 -0400

    Update parser.py
    Corrected some errors in rule handling due to incorrect array positioning. Still need to work on fixing the evaluation portion when walking through the tokens.

--- parser.py
+++ parser.py
@@ -22,7 +22,9 @@ class Sub(BinaryOp):
 class Mul(BinaryOp):
   def eval(self):
     return self.lhs.eval() * self.rhs.eval()
-
+class Div(BinaryOp):
+  def eval(self):
+    return self.lhs.eval() / self.rhs.eval()
 #Unary Operators  
 class UnaryOp(BaseBox):
   def __init__(self, lhs, rhs):
@@ -34,48 +36,57 @@ class Positive(UnaryOp):
 class Negate(UnaryOp):
   def eval(self):
     return -(self.lhs.eval())
+#Assignment Operator
+class Assignment():
+  def ():
+    return 
 
 #class Equal(BinaryOp):
  #   return self.rhs.eval()
 
 pg = ParserGenerator(
-["POSITIVE", "NEGATE","PLUS", "MINUS", "MULTIPLY", "LPAREN", "RPAREN", "NUMBER", "EQUAL", "IDENTIFIER"],
+["POSITIVE", "NEGATE","PLUS", "MINUS", "MULTIPLY", "DIVIDE", "LPAREN", "RPAREN", "NUMBER", "EQUAL", "IDENTIFIER"],
     precedence = [("left", ['POSITIVE', 'NEGATE']), ("left", ['MULTIPLY', 'DIVIDE']), ("left", ['PLUS', 'MINUS'])], cache_id="aaron_parser")
+
+@pg.production('main : expr')
+def main(p):
+  return p[0]
 
 @pg.production('expr : expr PLUS term')
 @pg.production('expr : expr MINUS term')
 def expr_binop(p):
-  left = p[0]
-  right = p[2]
-  if p[1].gettokentype()=='PLUS':
+  left = p[1]
+  right = p[3]
+  if p[2].gettokentype()=='PLUS':
     return Add(left,right)
-  elif p[1].getttokentype() == 'MINUS':
+  elif p[2].getttokentype() == 'MINUS':
     return Sub(left,right)
   else:
     raise AssertionError('Operation not defined %d', token.gettokentype())
 
-parser = pg.build()
-'''    
 @pg.production(' expr : term ')
 def expr_term(p):
-  return p[2]
+  return p[0]
 
-@pg.production('term MULTIPLY fact')
+@pg.production('term : term MULTIPLY fact')
+@pg.production('term : term DIVIDE fact')
 def term_mul(p):
-  left = p[0]
-  right = p[2]
-  if p[1].gettokentype()=="MULTIPLY":
+  left = p[1]
+  right = p[3]
+  if p[2].gettokentype()=="MULTIPLY":
     return Mul(left,right)
+  elif p[2].gettokentype()=="DIVIDE":
+    return Div(left,right)
   else:
     raise AssertionError('Binary Operation not defined %d', token.gettokentype())
   
 @pg.production('term : fact')
 def term_fact(p):
-  return p[2]
+  return p[0]
 
 @pg.production('fact : LPAREN expr RPAREN')
 def parenthesis(p):
-  return p[1]
+  return p[2]
 
 @pg.production('fact : NEGATE fact ')
 @pg.production('fact : POSITIVE fact')
@@ -89,28 +100,30 @@ def fact_unaryop(p):
   else:
     raise AssertionError('Unary Operation not defined %d', token.gettokentype())
     
-@pg.production('fact : Number')
+@pg.production('fact : NUMBER')
 def fact_number(p):
   return Number(int(p[0].getstr()))
 
 @pg.production('fact : IDENTIFIER')
 def fact_ID(p):
-  return 
+  return p[0]
+  
+@pg.production('expr : IDENTIFIER EQUAL expr')
+def expr_assign(p):
+  return p[0]
+  #return Assignment(Variable(p[1].getstr()),p[3])
   
 class BoxInt(BaseBox):
   def __init__(self, value):
     self.value = value
   def getint(self):
     return self.value
-'''
 
-#parser.parse(lexer.lex("let a = 5"), Environment()).to_string()
-list(lexer.lex("005+4"))
+class ParserState(object):
+  def __init__(self, filename):
+    self.filename = filename
+    
+parser = pg.build()
 
-lexer.input(data)
-
-while True:
-  tok = lexer.token()
-  if not tok:
-    break
-  print(tok.type, tok.value, tok.lineno, tok.lexpos)
+parser.parse(lexer.lex('x = 1'))
+#myparser.parse(lexer.lex(source), state=ParserState('foo.py'))
