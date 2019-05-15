e89dc44afda7c320a8a14505c942aaca5d39c613
Aaron Yam <ayam5005@gmail.com>
2019-05-15 17:07:09 -0400

    Update parser.py

--- parser.py
+++ parser.py
@@ -1,126 +1,115 @@
-from rply import ParserGenerator
+!pip install rply
+from rply import LexerGenerator
 from rply.token import BaseBox
-from lex import lexer
+from rply import ParserGenerator
 
-#PARSER
+lg = LexerGenerator()
+
+lg.add('NUMBER', r'\d+')
+lg.add('PLUS', r'\+')
+lg.add('MINUS', r'-')
+lg.add('MUL', r'\*')
+lg.add('DIV', r'/')
+lg.add('EQUAL', r'=')
+lg.add('OPEN_PARENS', r'\(')
+lg.add('CLOSE_PARENS', r'\)')
+lg.add("IDENTIFIER", r"[a-zA-Z_][a-zA-Z0-9_]*")
+
+lg.ignore('\s+')
+
+lexer = lg.build()
+
 class Number(BaseBox):
-  def __init__(self, value):
-    self.value = value
-  def eval(self):
-    return self.value
+    def __init__(self, value):
+        self.value = value
 
-#Binary Operators
+    def eval(self):
+        return self.value
+
 class BinaryOp(BaseBox):
-  def __init__(self, lhs, rhs):
-    self.lhs = lhs
-    self.rhs = rhs
+    def __init__(self, left, right):
+        self.left = left
+        self.right = right
+
 class Add(BinaryOp):
-  def eval(self):
-    return self.lhs.eval() + self.right.eval()
+    def eval(self):
+        return self.left.eval() + self.right.eval()
+
 class Sub(BinaryOp):
-  def eval(self):
-    return self.lhs.eval() - self.rhs.eval()
+    def eval(self):
+        return self.left.eval() - self.right.eval()
+
 class Mul(BinaryOp):
-  def eval(self):
-    return self.lhs.eval() * self.rhs.eval()
+    def eval(self):
+        return self.left.eval() * self.right.eval()
+
 class Div(BinaryOp):
-  def eval(self):
-    return self.lhs.eval() / self.rhs.eval()
-#Unary Operators  
-class UnaryOp(BaseBox):
-  def __init__(self, lhs, rhs):
-    self.lhs = lhs
-    self.rhs = rhs
-class Positive(UnaryOp):
-  def eval(self):
-    return +(self.lhs.eval())
-class Negate(UnaryOp):
-  def eval(self):
-    return -(self.lhs.eval())
+    def eval(self):
+        return self.left.eval() / self.right.eval()
 
-#class Equal(BinaryOp):
- #   return self.rhs.eval()
-
+class Equal(BinaryOp):
+    def eval(self):
+      temp_list_key = []
+      temp_list_value = []
+      temp_list_key.append(str(self.left))
+      temp_list_value.append(str(self.right.eval()))
+      for i in temp_list_key:
+        for k in temp_list_value:
+          symbol_table[i] = k
+            #symbol_table[self.left] = self.right.eval() 
+      return self.right.eval()
+      
 pg = ParserGenerator(
-["POSITIVE", "NEGATE","PLUS", "MINUS", "MULTIPLY", "DIVIDE", "LPAREN", "RPAREN", "NUMBER", "EQUAL", "IDENTIFIER"],
-    precedence = [("left", ['POSITIVE', 'NEGATE']), ("left", ['MULTIPLY', 'DIVIDE']), ("left", ['PLUS', 'MINUS'])], cache_id="aaron_parser")
+    # A list of all token names, accepted by the parser.
+    ['NUMBER', 'OPEN_PARENS', 'CLOSE_PARENS',
+     'PLUS', 'MINUS', 'MUL', 'DIV', 'IDENTIFIER', 'EQUAL'
+    ],
+    # A list of precedence rules with ascending precedence, to
+    # disambiguate ambiguous production rules.
+    precedence=[
+        ('left', ['PLUS', 'MINUS', 'EQUAL']),
+        ('left', ['MUL', 'DIV'])
+    ]
+)
 
-@pg.production('main : expr')
-def main(p):
-  return p[0]
+#dictionary for values
+symbol_table = { }
 
-@pg.production('expr : expr PLUS term')
-@pg.production('expr : expr MINUS term')
-def expr_binop(p):
-  left = p[1]
-  right = p[3]
-  if p[2].gettokentype()=='PLUS':
-    return Add(left,right)
-  elif p[2].getttokentype() == 'MINUS':
-    return Sub(left,right)
-  else:
-    raise AssertionError('Operation not defined %d', token.gettokentype())
+@pg.production('expression : NUMBER')
+def expression_number(p):
+    # p is a list of the pieces matched by the right hand side of the
+    # rule
+    return Number(int(p[0].getstr()))
 
-@pg.production(' expr : term ')
-def expr_term(p):
-  return p[0]
-
-@pg.production('term : term MULTIPLY fact')
-@pg.production('term : term DIVIDE fact')
-def term_mul(p):
-  left = p[1]
-  right = p[3]
-  if p[2].gettokentype()=="MULTIPLY":
-    return Mul(left,right)
-  elif p[2].gettokentype()=="DIVIDE":
-    return Div(left,right)
-  else:
-    raise AssertionError('Binary Operation not defined %d', token.gettokentype())
+@pg.production('expression : OPEN_PARENS expression CLOSE_PARENS')
+def expression_parens(p):
+    return p[1]
   
-@pg.production('term : fact')
-def term_fact(p):
-  return p[0]
+@pg.production('expression : expression PLUS expression')
+@pg.production('expression : expression MINUS expression')
+@pg.production('expression : expression MUL expression')
+@pg.production('expression : expression DIV expression')
+@pg.production('expression : IDENTIFIER EQUAL expression')
+def expression_binop(p):
+    left = p[0]
+    right = p[2]
+    if p[1].gettokentype() == 'PLUS':
+        return Add(left, right)
+    elif p[1].gettokentype() == 'MINUS':
+        return Sub(left, right)
+    elif p[1].gettokentype() == 'MUL':
+        return Mul(left, right)
+    elif p[1].gettokentype() == 'DIV':
+        return Div(left, right)
+    elif p[1].gettokentype() == 'EQUAL':
+        return Equal(left,right)
+    else:
+        raise AssertionError('Oops, this should not be possible!')
 
-@pg.production('fact : LPAREN expr RPAREN')
-def parenthesis(p):
-  return p[2]
-
-@pg.production('fact : NEGATE fact ')
-@pg.production('fact : POSITIVE fact')
-def fact_unaryop(p):
-  left = p[0]
-  right = p[1]
-  if p[0].gettokentype()=="POSITIVE":
-    return Positive(left,right)
-  elif p[0].gettokentype()=="NEGATE":
-    return Negate(left,right)
-  else:
-    raise AssertionError('Unary Operation not defined %d', token.gettokentype())
-    
-@pg.production('fact : NUMBER')
-def fact_number(p):
-  return Number(int(p[0].getstr()))
-
-@pg.production('fact : IDENTIFIER')
-def fact_ID(p):
-  return p[0]
-  
-@pg.production('expr : IDENTIFIER EQUAL expr')
-def expr_assign(p):
-  return ast.Assign(ast.IdentifierReference(p[0].getstr()), p[2])
-  #return Assignment(Variable(p[1].getstr()),p[3])
-  
-class BoxInt(BaseBox):
-  def __init__(self, value):
-    self.value = value
-  def getint(self):
-    return self.value
-
-class ParserState(object):
-  def __init__(self, filename):
-    self.filename = filename
-    
 parser = pg.build()
 
-parser.parse(lexer.lex('x = 1'))
-#myparser.parse(lexer.lex(source), state=ParserState('foo.py'))
+parser.parse(lexer.lex(' y = 75 + 50')).eval()
+parser.parse(lexer.lex('dq = 5')).eval()
+#symbol_table['xander'] = '3'
+for x in symbol_table:
+  print(x + ' = ' + symbol_table[x])
